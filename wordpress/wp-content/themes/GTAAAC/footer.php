f23071b528c2f67eb0896f29be8fbb50dd49a454
ajay <ajay786singh@gmail.com>
2013-09-23 22:53:33 +0530

    Footer Follow us section updated with new changes.

--- wordpress/wp-content/themes/GTAAAC/footer.php
+++ wordpress/wp-content/themes/GTAAAC/footer.php
@@ -12,9 +12,10 @@
 					</div>
 					<div class="footer-column-last">
 						<h3>Follow Us</h3>
-						<ul>
-                        	<li>Food and Farming <br /> <a href="https://twitter.com/GHfoodfarming" target="_blank">@GHFoodFarming</a></li>
-                        	<li>GTAAAC <br /> <a href="https://twitter.com/GTAAgAction" target="_blank">@GTAAgAction</a></li>
+						<h6>Find us on Twitter!</h6>
+                        <ul>
+                        	<li>Golden Horseshoe Food and Farming Alliance <br /> <a href="https://twitter.com/GHfoodfarming" target="_blank">@GHFoodFarming</a></li>
+                        	<li>Greater Toronto Area Agricultural Action Committee <br /> <a href="https://twitter.com/GTAAgAction" target="_blank">@GTAAgAction</a></li>
 							<!--<li class="facebook"><a href="https://twitter.com/#!/GTA__AAC">@GTA__AAC</a></li>
 							<li class="twitter"><a href="https://www.facebook.com/group.php?gid=110313392345265&v=wall">facebook</a></li>-->
 						</ul>
