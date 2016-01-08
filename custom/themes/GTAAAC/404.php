05f7859aba8ee13651617ae5fab898528ef876fb
Emily Dela Cruz <delacruz.emily@gmail.com>
2016-01-08 09:54:21 -0500

    Update: big search form on 404

--- custom/themes/GTAAAC/404.php
+++ custom/themes/GTAAAC/404.php
@@ -8,11 +8,11 @@
 
 	<div class="outer-container">
 		<div class="inner-container">
-			<h2>Try a search?</h2>
+			<h2>Try a search:</h2>
 	    	<div class="search-error-page">
 				<form role="search" method="get" class="search-form-error big-search" action="<?php echo home_url( '/' ); ?>">
 					<label>
-						<input type="search" class="search-field big-search" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+						<input type="search" class="search-field big-search" placeholder="<?php echo esc_attr_x( 'What are you looking for?', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
 					</label>
 					<input type="submit" class="error-submit big-search" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
 				</form>
